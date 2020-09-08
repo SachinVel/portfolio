1bed16c00f0a79bac0ea61b0d4bdbaefcde8449f
Sachin Velmurugan <sachin.v@zohocorp.com>
2020-09-08 20:10:08 +0530

    Updates

---
+++ precache-manifest.96c33c82d693aa45948f25481731ad55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebae98f6c4767661bca99e36e2ad437a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f5908415c1215aca9909",
+    "url": "/portfolio/static/css/main.c3deb132.chunk.css"
+  },
+  {
+    "revision": "6268c68714ff7eac5440",
+    "url": "/portfolio/static/js/2.ffca60fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ffca60fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5908415c1215aca9909",
+    "url": "/portfolio/static/js/main.47dfeeb4.chunk.js"
+  },
+  {
+    "revision": "6465fce3a27ba2717a2c",
+    "url": "/portfolio/static/js/runtime-main.60dad8f2.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "570c230d83bdff3c88866ec7fc8cca25",
+    "url": "/portfolio/static/media/face-image.570c230d.png"
+  },
+  {
+    "revision": "a5a80e337f01a411b29570d41a6523eb",
+    "url": "/portfolio/static/media/naruto-minimal-wallpaper.a5a80e33.png"
+  },
+  {
+    "revision": "73fedd912ba11f583a359ce4c2e742ed",
+    "url": "/portfolio/static/media/njnaruto.73fedd91.ttf"
+  }
+]);
